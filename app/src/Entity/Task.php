f651041067896a5dce4de2778da69b1a7396838e
unknown <akbiyik.ilker7@gmail.com>
2022-04-17 12:12:14 +0200

    new project & running postgres on docker

---
+++ app/src/Entity/Task.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $difficulty;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $timeToLearn;
+
+    #[ORM\Column(type: 'integer')]
+    private $priority;
+
+    #[ORM\Column(type: 'string', length: 1500, nullable: true)]
+    private $notes;
+
+    #[ORM\Column(type: 'integer')]
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(int $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getTimeToLearn(): ?int
+    {
+        return $this->timeToLearn;
+    }
+
+    public function setTimeToLearn(?int $timeToLearn): self
+    {
+        $this->timeToLearn = $timeToLearn;
+
+        return $this;
+    }
+
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(int $priority): self
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(?string $notes): self
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getState(): ?int
+    {
+        return $this->state;
+    }
+
+    public function setState(int $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-let tailwindcss = require("tailwindcss");
-
-module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    require('postcss-import'),
-    require('autoprefixer')
-  ]
-}
