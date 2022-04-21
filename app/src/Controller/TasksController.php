5c3b7cc9d9280a995a63c0c6f9edb62874b35339
unknown <akbiyik.ilker7@gmail.com>
2022-04-21 12:30:12 +0200

    tasks created & working on getting tasks

--- app/src/Controller/TasksController.php
+++ app/src/Controller/TasksController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Tasks;
+use App\Entity\User;
 use App\Form\TaskFormType;
 use App\Repository\TasksRepository;
+use Symfony\Component\Security\Core\Security;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +16,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TasksController extends AbstractController
 {   
+    /**
+     * @var Security
+     */
+    private $security;
     private $em;
 
-    public function __construct(EntityManagerInterface $em, TasksRepository $taskRepository)
+    public function __construct(EntityManagerInterface $em, TasksRepository $taskRepository, Security $security)
     {
+        $this->security = $security;
         $this->em = $em;
         $this->taskRepository = $taskRepository;
     }
@@ -25,22 +32,48 @@ class TasksController extends AbstractController
     #[Route('/tasks', name: 'app_tasks')]
     public function index(): Response
     {
+
+        $securityUser = $this->security->getUser();
+        if(!$securityUser) {
+            return $this->redirectToRoute('app_login');
+        }
+        $id = $securityUser->getId();
+        
+        $tasks = $this->taskRepository->findBy(['id'=> $id]);
+        
+        dd($tasks);
+
         return $this->render('tasks/index.html.twig', [
             'controller_name' => 'TasksController',
         ]);
     }
 
     #[Route('/tasks/create', name: 'app_create_task')]
-    public function create(Request $request): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {   
+        $securityUser = $this->security->getUser();
         $task = new Tasks();
         $form = $this->createForm(TaskFormType::class, $task);
         
         $form->handleRequest($request);
+
+        
+        if(!$securityUser) {
+            return $this->redirectToRoute('app_login');
+        }
+
         if ($form->isSubmitted() && $form->isValid()) {
+            
             $newTask = $form->getData();
-            dd($newTask);
-            exit;
+            $newTask->setUserId($securityUser);
+
+            $em->persist($newTask);
+            $em->flush();
+
+            // dd($newTask);
+            // dd($securityUser);
+            // exit;
+            return $this->redirectToRoute('app_tasks');
         }
 
 
